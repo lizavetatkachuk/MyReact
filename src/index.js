3461ebec8e2ca4c9b8f122bc28ac4337636faaf7
Lizaveta <elis.tkachuk@gmail.com>
2018-09-06 14:09:33 +0300

    shows the lish of friends and adds a new one

--- src/index.js
+++ src/index.js
@@ -4,5 +4,83 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class FriendsContainer extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name: 'Tyler McGinnis',
+            friends: [
+                'Jake Lingwall',
+                'Sarah Drasner',
+                'Merrick Christensen'
+            ],
+        }
+
+        this.addFriend = this.addFriend.bind(this)
+    }
+    addFriend(friend) {
+        this.setState((state) => ({
+            friends: state.friends.concat([friend])
+        }))
+    }
+    render() {
+        return (
+            <div>
+                <h3> Name: {this.state.name} </h3>
+                <AddFriend addNew={this.addFriend} />
+                <ShowList names={this.state.friends} />
+            </div>
+        )
+    }
+}
+class AddFriend extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            newFriend: ''
+        }
+
+        this.updateNewFriend = this.updateNewFriend.bind(this)
+        this.handleAddNew = this.handleAddNew.bind(this)
+    }
+    updateNewFriend(e) {
+        this.setState({
+            newFriend: e.target.value
+        })
+    }
+    handleAddNew() {
+        this.props.addNew(this.state.newFriend)
+        this.setState({
+            newFriend: ''
+        })
+    }
+    render() {
+        return (
+            <div>
+                <input
+                    type="text"
+                    value={this.state.newFriend}
+                    onChange={this.updateNewFriend}
+                />
+                <button onClick={this.handleAddNew}> Add Friend </button>
+            </div>
+        )
+    }
+}
+class ShowList extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3> Friends </h3>
+                <ul>
+                    {this.props.names.map((friend) => {
+                        return <li> {friend} </li>
+                    })}
+                </ul>
+            </div>
+        )
+    }
+}
+ReactDOM.render(<FriendsContainer />, document.getElementById('root'));
